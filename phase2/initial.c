4f43e8dd3ce0e00f6e0fa20250867e389676678f
oghapkim <kim_o1@denison.edu>
2025-02-15 19:19:54 -0500

    phase 2 set up

--- phase2/initial.c
+++ phase2/initial.c
@@ -1,5 +1,6 @@
 /*********************************INITIAL.C*******************************
  *
+ *      Initializes global data structures
  * 
  *      Modified by Phuong and Oghap on Feb 2025
  */
@@ -10,3 +11,10 @@
 #include "../h/asl.h"
 #include "../h/types.h"
 
+/* Global Variables*/
+
+int         process_count;      /* Number of started processes*/
+int         softBlock_count;    /* Number of started that are in blocked*/
+pcb_PTR     readyQ;             /* Tail ptr to a queue of pcbs that are ready*/
+pcb_PTR     currentP;           /* Pointer to the pcb that is in the “running” state*/
+/* Device Semaphore*/
