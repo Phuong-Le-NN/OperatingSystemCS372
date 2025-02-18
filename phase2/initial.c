4124c1a0e117597ce482fc5adbcf5801cc647cae
Phuong-Le-NN <le_p2@denison.edu>
2025-02-17 21:27:09 -0500

    minor change

--- phase2/initial.c
+++ phase2/initial.c
@@ -13,8 +13,12 @@
 
 /* Global Variables*/
 
-int         process_count;      /* Number of started processes*/
-int         softBlock_count;    /* Number of started that are in blocked*/
-pcb_PTR     readyQ;             /* Tail ptr to a queue of pcbs that are ready*/
-pcb_PTR     currentP;           /* Pointer to the pcb that is in the “running” state*/
-/* Device Semaphore*/
+void main() {
+    int         process_count = 0;                          /* Number of started processes*/
+    int         softBlock_count = 0;                        /* Number of started that are in blocked*/
+    pcb_PTR     readyQ = mkEmptyProcQ();                    /* Tail ptr to a queue of pcbs that are ready*/
+    xxx->tlb_refll_handler = (memaddr) uTLB_RefillHandler;  /*uTLB RefillHandler replaced later*/
+    pcb_PTR     currentP = NULL;                            /* Pointer to the pcb that is in the “running” state*/
+    /* Device Semaphore*/
+}
+
