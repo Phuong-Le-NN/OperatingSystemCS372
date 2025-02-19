800949873a1561b5229148a7d75ab2e7cd75b893
Phuong-Le-NN <le_p2@denison.edu>
2025-02-19 18:42:33 -0500

    scheduler.c in progress

--- phase2/scheduler.c
+++ phase2/scheduler.c
@@ -4,7 +4,11 @@
  *      Modified by Phuong and Oghap on Feb 2025
  */
 
-
+#include "../h/pcb.h"
+#include "../h/asl.h"
+#include "../h/types.h"
+#include "initial.c"
+#include "/usr/include/umps3/umps/libumps.h"
 
 /*
 
@@ -14,3 +18,20 @@ In scheduler:
     - then, LDST(runningProcQ -> p_state)
 
 */
+
+void scheduler (){
+    /*if the ready Q is empty*/
+    if (emptyProcQ(readyQ)){
+        if (process_count == 0) {
+            HALT();
+        }
+        if (process_count > 0 && softBlock_count > 0){
+            setSTATUS();
+            WAIT();
+        }
+    }
+    
+    currentP = removeBlocked(readyQ);
+    LDIT(5); /*Load 5 milisec on the PLT*/
+    LDST(&(currentP->p_s)); /*pass in the address of current process processor state*/
+}
