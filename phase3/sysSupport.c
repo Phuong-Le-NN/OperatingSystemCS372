44d27bb5b0397c39a0ea469b28671bdf58056c1a
Phuong Le <le_p2@denison.edu>
2025-03-22 04:56:32 +0000

    syscall handler and general exception handler

--- phase3/sysSupport.c
+++ phase3/sysSupport.c
@@ -19,3 +19,43 @@
 #include "../phase2/scheduler.h"
 #include "../phase2/exceptions.h"
 #include "../phase2/interrupts.h"
+
+void helper_return_control(support_t *savedExcState){
+    savedExcState->sup_exceptState[GENERALEXCEPT].s_pc += 4;
+    LDST(&(savedExcState->sup_exceptState[GENERALEXCEPT]));
+}
+
+void program_trap_handler(){
+
+}
+
+void syscall_handler(support_t *savedExcState) {
+    switch (savedExcState->sup_exceptState[GENERALEXCEPT].s_a0){
+        case 9:
+            TERMINATE();
+            helper_return_control(savedExcState);
+        case 10:
+            GET_TOD();
+            helper_return_control(savedExcState);
+        case 11:
+            WRITE_TO_PRINTER(savedExcState);
+            helper_return_control(savedExcState);
+        case 12:
+            WRITE_TO_TERMINAL();
+            helper_return_control(savedExcState);
+        case 13:
+            READ_FROM_TERMINAL();
+            helper_return_control(savedExcState);
+        default: /*should never reach this case*/
+            program_trap_handler();
+            helper_return_control(savedExcState);
+    }
+}
+
+void general_exception_handler() { 
+    support_t *savedExcState = SYSCALL(8, 0, 0, 0);
+    if (savedExcState->sup_exceptState[GENERALEXCEPT].s_a0 >= 9 && savedExcState->sup_exceptState[GENERALEXCEPT].s_a0 <= 13){
+        syscall_handler(savedExcState);
+    }
+        program_trap_handler();
+}
