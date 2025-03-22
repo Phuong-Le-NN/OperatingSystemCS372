5a4447d8fd25b885714155d24c92d7328d9c01c2
Phuong Le <le_p2@denison.edu>
2025-03-22 19:36:39 +0000

    write terminal

--- phase3/sysSupport.c
+++ phase3/sysSupport.c
@@ -53,6 +53,7 @@ void WRITE_TO_PRINTER(support_t *passedUpSupportStruct) {
     /* add dev mutex sema4 once init proc is done*/
     int i;
     for (i = 0; i < savedExcState.s_a2; i++){
+        setSTATUS(getSTATUS() & (~IECBITON));
         printerDevAdd->d_data0 = *((char *) (savedExcState.s_a1 + i)); /*calculate address and accessing the current char*/
         printerDevAdd->d_command = 2; /* command PRINTCHR */
         SYSCALL(5, PRNTINT, devNo, 0); /*call SYSCALL WAITIO to block until interrupt*/
@@ -60,12 +61,54 @@ void WRITE_TO_PRINTER(support_t *passedUpSupportStruct) {
             savedExcState.s_v0 = - printerDevAdd->d_status;
             return;
         }
+        setSTATUS(getSTATUS() | IECBITON);
     }
 
     if (printerDevAdd->d_status == 1) { /* "Device Ready" */
         savedExcState.s_v0 = savedExcState.s_a2;;
     } else {
         savedExcState.s_v0 = - printerDevAdd->d_status;
+    }
+}
+
+void WRITE_TO_TERMINAL(support_t *passedUpSupportStruct) {
+    /*
+    virtual address of the first character of the string to be transmitted in a1,
+    the length of this string in a2
+    */
+    state_t savedExcState = passedUpSupportStruct->sup_exceptState[GENERALEXCEPT];
+
+    int devNo = passedUpSupportStruct->sup_asid - 1;
+    device_t *termDevAdd = devAddrBase(TERMINT, devNo);
+
+    /* Error: to write to a printer device from an address outside of the requesting U-proc’s logical address space*/
+    int stringOutsideAddSpace = (savedExcState.s_a1 < KSEG2 | savedExcState.s_a1 > KUSEG)? TRUE:FALSE;
+    /* Error: length less than 0*/
+    int negStringLen = (savedExcState.s_a2 < 0)? TRUE:FALSE;
+    /* Error: a length greater than 128*/
+    int oversizeStringLen = (savedExcState.s_a2 > 128)? TRUE:FALSE;
+
+    if (stringOutsideAddSpace || negStringLen || oversizeStringLen){
+        SYSCALL(9, 0, 0, 0);
+    }
+
+    /* add dev mutex sema4 once init proc is done*/
+    int i;
+    for (i = 0; i < savedExcState.s_a2; i++){
+        setSTATUS(getSTATUS() & (~IECBITON));
+        termDevAdd->t_transm_command = *((char *) (savedExcState.s_a1 + i)) << 7 + 2; /*calculate address and accessing the current char, shift to the right position and add the TRANSMITCHAR command*/
+        SYSCALL(5, TERMINT, devNo, FALSE); /*call SYSCALL WAITIO to block until interrupt*/
+        if (termDevAdd->t_transm_status != 5) { /* operation ends with a status other than "Device Ready" */
+            savedExcState.s_v0 = - termDevAdd->t_transm_status;
+            return;
+        }
+        setSTATUS(getSTATUS() | IECBITON);
+    }
+
+    if (termDevAdd->t_transm_status == 5) { /* "Device Ready" */
+        savedExcState.s_v0 = savedExcState.s_a2;;
+    } else {
+        savedExcState.s_v0 = - termDevAdd->t_transm_status;
     }
 }
 
@@ -81,7 +124,7 @@ void syscall_handler(support_t *passedUpSupportStruct) {
             WRITE_TO_PRINTER(passedUpSupportStruct);
             helper_return_control(passedUpSupportStruct);
         case 12:
-            WRITE_TO_TERMINAL();
+            WRITE_TO_TERMINAL(passedUpSupportStruct);
             helper_return_control(passedUpSupportStruct);
         case 13:
             READ_FROM_TERMINAL();
