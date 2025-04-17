77a5b5eec6dfff85a5ad4890407279ccdb8578ae
oghapkim <kim_o1@denison.edu>
2025-04-16 21:06:04 -0400

    simple test added to testers

---
+++ phase3/testers/test_oghap.c
@@ -0,0 +1,34 @@
+/* Simple test that asks user input to check printer and terminal is working */
+
+#include "h/localLibumps.h"
+#include "h/tconst.h"
+#include "h/print.h"
+
+void main() {
+	
+    int status, i;
+	char buf[20];
+	char buf2[20];
+
+	print(WRITETERMINAL, "Simple Printer Test starts\n");
+	print(WRITETERMINAL, "Write something to print: ");
+		
+	status = SYSCALL(READTERMINAL, (int)&buf[0], 0, 0);
+	buf[status] = EOS;
+	
+	print(WRITETERMINAL, "\n");
+
+    i = 0;
+	for( i = 0; i < status-1; i++ )
+	{
+		buf2[i] = buf[i];
+	}
+
+    buf2[status - 1] = EOS;
+
+    print(WRITETERMINAL, &buf2[0]);
+	
+	print(WRITETERMINAL, "\n\nInput String Printed \n");
+	
+	SYSCALL(TERMINATE, 0, 0, 0);
+}
